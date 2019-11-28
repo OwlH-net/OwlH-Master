d5baca54f3d1615077977f177d2bcd2b5598bf9e
OwlH Jizquierdo <master@owlh.net>
2019-11-28 14:28:59 +0100

    TLS supported ciphers

--- main.go
+++ main.go
@@ -89,8 +89,8 @@ func main() {
 
 
     beego.BeeApp.Server.TLSConfig = &tls.Config{    CipherSuites: []uint16{
-                                                        tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-                                                        tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+                                                        tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+                                                        tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
                                                     },
                                                     MinVersion:               tls.VersionTLS12,
                                                     PreferServerCipherSuites: true,
