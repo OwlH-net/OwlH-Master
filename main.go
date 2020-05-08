5ed030943a10d1a76b1cf2f1cfa2a41b203141d8
sortizowlh <sortiz@owlh.net>
2020-05-08 09:57:58 +0200

    Permissions bugs. Testing...

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ import (
 func main() {
     
     //Application version
-    logs.Info("Version OwlH Master: 0.12.0.20200504")
+    logs.Info("Version OwlH Master: 0.12.0.20200508")
     utils.Load()
 
     //get logger data
